d7e2c8bbb884b0760bc4057d67cc3ca0ada4e84d
YurijM <y-myagkov@mail.ru>
2019-01-21 19:05:30 +0300

    Для министерской проверки изменена страница просмотра портфолио студентов преподавателем.

--- application/classes/controller/cabinet/portfolio.php
+++ application/classes/controller/cabinet/portfolio.php
@@ -19,7 +19,7 @@ class Controller_Cabinet_Portfolio extends Controller_Cabinet
 
 		$portfolio = View::factory('cabinet/v_portfolio');
 
-		$portfolio->no = 1;
+		/*$portfolio->no = 1;
 
 		$portfolio->students = DB::select('s.id', 's.person', 's.group', ['count("a.student_id")', 'count'])
 			->from(['student_achievements', 'a'])
@@ -28,7 +28,10 @@ class Controller_Cabinet_Portfolio extends Controller_Cabinet
 			->group_by('a.student_id')
 			->order_by('s.person')
 			//->distinct('s.id')
-			->execute();
+			->execute();*/
+
+		$portfolio->student = $this->request->param('student');
+		$portfolio->isAbsent = $this->request->param('isAbsent');
 
 		$this->cabinet->cabinet = $portfolio;
 		$this->template->main = $this->cabinet;
@@ -37,7 +40,7 @@ class Controller_Cabinet_Portfolio extends Controller_Cabinet
 	//==========================================================================//
 	public function action_view()
 	{
-		$studentId = $this->request->param('id');
+		/*$studentId = $this->request->param('id');
 
 		$student = ORM::factory('student', $studentId)->person;
 
@@ -51,6 +54,35 @@ class Controller_Cabinet_Portfolio extends Controller_Cabinet
 
 		$portfolio = View::factory('cabinet/v_portfolio_list', $data);
 
+		$this->cabinet->cabinet = $portfolio;
+		$this->template->main = $this->cabinet;*/
+
+
+		$student = Arr::get($_POST, 'student');
+
+		$studentId = ORM::factory('student')->where('person', '=', $student)->find()->id;
+
+		$isAchivement = ORM::factory('studentachievements')
+			->where('student_id', '=', $studentId)
+			->find()
+			->id;
+
+		if ($isAchivement) {
+			$this->cabinet->pageTitle = 'Портфолио - ' . $student;
+
+			$data['dirDoc'] = substr(ORM::factory('setting', ['key' => 'dir_docs_student_achievements'])->value, 1);
+			$data['achievements'] = ORM::factory('studentachievements')
+				->where('student_id', '=', $studentId)
+				->order_by('description')
+				->find_all();
+
+			$portfolio = View::factory('cabinet/v_portfolio_list', $data);
+		} else {
+			$data['student'] = $student;
+			$data['isAbsent'] = ($student == '' ? false : true);
+
+			$portfolio = View::factory('cabinet/v_portfolio', $data);
+		}
 		$this->cabinet->cabinet = $portfolio;
 		$this->template->main = $this->cabinet;
 	}
--- application/views/cabinet/v_portfolio.php
+++ application/views/cabinet/v_portfolio.php
@@ -1,4 +1,24 @@
-<div class="table">
+<div class="col-md-offset-4 col-sm-offset-3 col-xs-offset-2 col-md-4 col-sm-6 col-xs-8">
+	<?= Form::open('cabinet/portfolio/view') ?>
+	<div class="form-group">
+		<?= Form::label('student', 'Студент') ?>
+		<?= Form::input('student', $student, ['class' => 'form-control', 'placeholder' => 'Фамилия и инициалы студента', 'autofocus']) ?>
+	</div>
+
+	<? if ($isAbsent): ?>
+		<div class="alert alert-danger text-center">
+			Студент <b><?= $student ?></b> отсутствует в списке
+		</div>
+	<? endif ?>
+
+	<div class="form-submit text-center">
+		<?= Form::submit('search', 'Выбрать', ['class' => 'btn btn-success btn-sm']) ?>
+	</div>
+	<?= Form::close() ?>
+</div>
+
+
+<!--<div class="table">
 	<table class="table-responsive table-condensed table-bordered" style="margin: 10px auto">
 		<tr>
 			<th>№ п/п</th>
@@ -6,25 +26,25 @@
 			<th>Группа</th>
 			<th>Кол-во</th>
 		</tr>
-		<? foreach ($students as $student): ?>
+		<? /* foreach ($students as $student): */ ?>
 			<tr>
 				<td class="text-center">
-					<?= $no++ ?>
+					<? /*= $no++ */ ?>
 				</td>
 				<td class="anchor">
-					<?= HTML::anchor(
+					<? /*= HTML::anchor(
 						'cabinet/portfolio/view/' . $student['id'],
 						$student['person'],
 						['title' => 'Просмотреть портфолио']
-					) ?>
+					) */ ?>
 				</td>
 				<td class="text-center">
-					<?= $student['group'] ?>
+					<? /*= $student['group'] */ ?>
 				</td>
 				<td class="text-center">
-					<?= $student['count'] ?>
+					<? /*= $student['count'] */ ?>
 				</td>
 			</tr>
-		<? endforeach ?>
+		<? /* endforeach */ ?>
 	</table>
-</div>
+</div>-->
