6d9924029929a8b0dbcf4c3147840f3c52a50d00
YurijM <y-myagkov@mail.ru>
2020-01-27 12:10:55 +0300

    Изменёно содержимое раздела "Вакансии"

--- application/views/v_vacant.php
+++ application/views/v_vacant.php
@@ -180,74 +180,81 @@
 				<td class="text-center">0</td>
 				<td class="text-center">0</td>
 			</tr>
-		</table>
-	</div>
-
-
-	<div class="table table-responsive">
-		<table class="table-responsive table-bordered table-condensed">
-			<tr>
-				<th colspan="3">Наименование образовательной программы, направления подготовки</th>
-				<th colspan="4">Количество вакантных мест для приема (перевода)</th>
+			<tr style="background-color: #d0e0f7">
+				<td class="text-center">38.03.01</td>
+				<td class="text-center">Экономика</td>
+				<td class="text-center">Высшее образование - бакалавриат</td>
+				<td class="text-center">1</td>
+				<td class="text-center">заочная</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
 			</tr>
-			<tr>
-				<th>Шифр</th>
-				<th colspan="2">Наименование</th>
-				<th>За счет бюджетных ассигнований федерального бюджета</th>
-				<th>За счет бюджетных ассигнований бюджетов субъекта Российской Федерации</th>
-				<th>За счет бюджетных ассигнований местных бюджетов</th>
-				<th>За счет средств физических и (или) юридических лиц</th>
+			<tr class="bg-info">
+				<td class="text-center" rowspan="2">38.03.01</td>
+				<td class="text-center" rowspan="2">Экономика</td>
+				<td class="text-center" rowspan="2">Высшее образование - бакалавриат</td>
+				<td class="text-center" rowspan="2">2</td>
+				<td class="text-center">очная</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+				<td class="text-center">1</td>
 			</tr>
-			<tr>
-				<td class="text-center" colspan="7"><strong>Бакалавриат прикладной</strong></td>
+			<tr class="bg-info">
+				<td class="text-center">заочная</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+				<td class="text-center">2</td>
 			</tr>
-			<tr>
-				<td class="text-center" rowspan="2"><strong>08.03.01</strong></td>
-				<td rowspan="2"><strong>Строительство</strong></td>
-				<td class="text-center">очно</td>
-				<td class="text-center">-</td>
-				<td class="text-center">-</td>
-				<td class="text-center">-</td>
+			<tr style="background-color: #d0e0f7">
+				<td class="text-center" rowspan="2">38.03.01</td>
+				<td class="text-center" rowspan="2">Экономика</td>
+				<td class="text-center" rowspan="2">Высшее образование - бакалавриат</td>
+				<td class="text-center" rowspan="2">3</td>
+				<td class="text-center">очная</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+				<td class="text-center">1</td>
+			</tr>
+			<tr style="background-color: #d0e0f7">
+				<td class="text-center">заочная</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+				<td class="text-center">2</td>
+			</tr>
+			<tr class="bg-info">
+				<td class="text-center" rowspan="2">38.03.01</td>
+				<td class="text-center" rowspan="2">Экономика</td>
+				<td class="text-center" rowspan="2">Высшее образование - бакалавриат</td>
+				<td class="text-center" rowspan="2">4</td>
+				<td class="text-center">очная</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+			</tr>
+			<tr class="bg-info">
+				<td class="text-center">заочная</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+				<td class="text-center">2</td>
+			</tr>
+			<tr style="background-color: #d0e0f7">
+				<td class="text-center">38.03.01</td>
+				<td class="text-center">Экономика</td>
+				<td class="text-center">Высшее образование - бакалавриат</td>
 				<td class="text-center">5</td>
-			</tr>
-			<tr>
-				<td class="text-center">заочно</td>
-				<td class="text-center">-</td>
-				<td class="text-center">-</td>
-				<td class="text-center">-</td>
-				<td class="text-center">7</td>
-			</tr>
-			<tr>
-				<td class="text-center" rowspan="2"><strong>09.03.01</strong></td>
-				<td rowspan="2"><strong>Информатика и вычислительная техника</strong></td>
-				<td class="text-center">очно</td>
-				<td class="text-center">-</td>
-				<td class="text-center">-</td>
-				<td class="text-center">-</td>
-				<td class="text-center">16</td>
-			</tr>
-			<tr>
-				<td class="text-center">заочно</td>
-				<td class="text-center">-</td>
-				<td class="text-center">-</td>
-				<td class="text-center">-</td>
-				<td class="text-center">-</td>
-			</tr>
-			<tr>
-				<td class="text-center" rowspan="2"><strong>38.03.01</strong></td>
-				<td rowspan="2"><strong>Экономика</strong></td>
-				<td class="text-center">очно</td>
-				<td class="text-center">-</td>
-				<td class="text-center">-</td>
-				<td class="text-center">-</td>
-				<td class="text-center">3</td>
-			</tr>
-			<tr>
-				<td class="text-center">заочно</td>
-				<td class="text-center">-</td>
-				<td class="text-center">-</td>
-				<td class="text-center">-</td>
-				<td class="text-center">5</td>
+				<td class="text-center">заочная</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
+				<td class="text-center">0</td>
 			</tr>
 		</table>
 	</div>
