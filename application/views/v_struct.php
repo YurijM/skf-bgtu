501b3669fd431771a153d338cdb8a31afac78e2e
YurijM <y-myagkov@mail.ru>
2020-01-30 16:48:40 +0300

    Изменена микроразметка в разделе "Структура и органы управления"

--- application/views/v_struct.php
+++ application/views/v_struct.php
@@ -7,29 +7,47 @@
       <? if ($structure_id > 0): ?>
         </ul>
       <? endif ?>
-        
-      <p>
-        <strong itemprop="name"><?=$item->structure->structure?></strong>
-        <?= ($item->structure->doc && $item->structure->file_doc ? 
-          HTML::anchor($dir_docs_structure.$item->structure->file_doc, ' ('.$item->structure->doc.')', ['target' => '_blank']) : '')
-        ?>
-      </p>
+
+      <div>
+				<div style="display: inline" itemprop="name"><strong><?=$item->structure->structure?></strong></div>
+				<?= (
+				$item->structure->doc && $item->structure->file_doc
+					? '<div style="display: inline" itemprop="divisionClauseDocLink">'
+					.HTML::anchor(
+						$dir_docs_structure.$item->structure->file_doc,
+						' ('.$item->structure->doc.')',
+						['target' => '_blank']
+					).'</div>'
+					: '<div class="hidden" itemprop="divisionClauseDocLink">документа нет</div>'
+				) ?>
+      </div>
+
       <ul type="disc">
     <? endif ?>
     <li>
       <?=
-        '<span itemprop="fio">'.$item->personnel->family.' '.$item->personnel->name.' '
-        .$item->personnel->patronymic
-        .'</span>'
+        '<div itemprop="fio">'
+        .$item->personnel->family.' '.$item->personnel->name.' '.$item->personnel->patronymic
         .($item->post ? ' - '.$item->post : '')
-        .($item->phone ? ',<br>телефон(ы): '.$item->phone : '')
-        .($item->email ? ',<br>e-mail: '.'<span itemprop="email">'.$item->email.'</span>' : '')
-        .($item->location ? ',<br>адрес: '.'<span itemprop="addressStr">'.$item->location.'</span>' : '')
+				.'</div>'
+        .($item->phone ? '<div>телефон(ы): '.$item->phone.'</div>' : '')
+        .($item->email
+	        ? '<div itemprop="email">e-mail: '.$item->email.'</div>'
+	        : '<div class="hidden" itemprop="email">e-mail нет</div>'
+        )
+        .($item->location
+	        ? '<div itemprop="addressStr">адрес: '.$item->location.'</div>'
+	        : '<div class="hidden" itemprop="addressStr">адреса нет</div>'
+        )
       ?>
+	    <div class="hidden" itemprop="site">сайта нет</div>
     </li>
     <? $structure_id = $item->structure_id ?>
   <? endforeach ?>
   </ul>
 
-  <?=HTML::image($dir_img.'structure.jpg', ['class' => 'img img-responsive center-block', 'alt' => $page_title])?>
+  <?=HTML::image($dir_img.'structure.jpg', [
+  	'class' => 'img img-responsive center-block',
+	  'alt' => $page_title
+  ])?>
 </div>
