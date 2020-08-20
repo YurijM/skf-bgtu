bf2649f4159fadffa587cd16c24b1bdbee94d1f8
YurijM <y-myagkov@mail.ru>
2020-08-20 21:12:45 +0300

    Добавлен новый раздел "Наука - Информационная безопасность"

---
+++ application/classes/controller/infosecurity.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Infosecurity extends Controller_Base {
+	public function action_index()
+  {
+    $this->template->page_title = 'Информационная безопасность';
+
+    $infosecurity = View::factory('v_info_security');
+
+		$infosecurity->mode = $this->mode;
+		$infosecurity->page_title = $this->template->page_title;
+		$infosecurity->dir_docs_info_security = ORM::factory('setting', array('key' => 'dir_docs_info_security'))->value;
+
+    $this->template->main = $infosecurity;
+  }
+}
