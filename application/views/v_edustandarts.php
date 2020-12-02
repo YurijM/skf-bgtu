02d857a7c12bcf27ca9e41d680a0b44ae2095817
YurijM <y-myagkov@mail.ru>
2020-12-02 12:22:09 +0300

    Техзадание от 01.12.2020 11:08

--- application/views/v_edustandarts.php
+++ application/views/v_edustandarts.php
@@ -5,18 +5,78 @@
     Федеральные образовательные стандарты высшего образования (уровень бакалавриата):
   </p>
   
-  <ul type="disc" itemprop="eduDoc">
+  <!--<ul type="disc" itemprop="eduDoc">
     <li>
-      <?=Html::anchor($dir_docs_educational_standard.'standart_08.03.01_so.pdf', '08.03.01 Строительство',
-        ['target' => '_blank', 'itemprop' => 'eduFedDoc'])?>;
+      <?/*=Html::anchor($dir_docs_educational_standard.'standart_08.03.01_so.pdf', '08.03.01 Строительство',
+        ['target' => '_blank', 'itemprop' => 'eduFedDoc'])*/?>;
     </li>
     <li>
-      <?=Html::anchor($dir_docs_educational_standard.'standart_09.03.01_it.pdf', '09.03.01 Информационные системы и технологии',
-        ['target' => '_blank', 'itemprop' => 'eduFedDoc'])?>;
+      <?/*=Html::anchor($dir_docs_educational_standard.'standart_09.03.01_it.pdf', '09.03.01 Информационные системы и технологии',
+        ['target' => '_blank', 'itemprop' => 'eduFedDoc'])*/?>;
     </li>
     <li>
-      <?=Html::anchor($dir_docs_educational_standard.'standart_38.03.01_ek.pdf', '38.03.01 Экономика',
-        ['target' => '_blank', 'itemprop' => 'eduFedDoc'])?>
+      <?/*=Html::anchor($dir_docs_educational_standard.'standart_38.03.01_ek.pdf', '38.03.01 Экономика',
+        ['target' => '_blank', 'itemprop' => 'eduFedDoc'])*/?>
     </li>
-  </ul>
+  </ul>-->
+
+	<ul type="disc" itemprop="eduDoc">
+		<li>
+			<?=Html::anchor(
+				$dir_docs_educational_standard.'so-08.03.01-2015.pdf',
+				'08.03.01 Строительство (2015)',
+				['target' => '_blank', 'itemprop' => 'eduFedDoc'])?>;
+		</li>
+		<li>
+			<?=Html::anchor(
+				$dir_docs_educational_standard.'so-08.03.01-2017.pdf',
+				'08.03.01 Строительство (2017)',
+				['target' => '_blank', 'itemprop' => 'eduFedDoc'])?>;
+		</li>
+	</ul>
+
+	<ul type="disc" itemprop="eduDoc">
+		<li>
+			<?=Html::anchor(
+				$dir_docs_educational_standard.'it-09.03.01-2016.pdf',
+				'09.03.01 Информатика и вычислительная техника (2016)',
+				['target' => '_blank', 'itemprop' => 'eduFedDoc'])?>;
+		</li>
+		<li>
+			<?=Html::anchor(
+				$dir_docs_educational_standard.'it-09.03.01-2017.pdf',
+				'09.03.01 Информатика и вычислительная техника (2017)',
+				['target' => '_blank', 'itemprop' => 'eduFedDoc'])?>;
+		</li>
+	</ul>
+
+	<ul type="disc" itemprop="eduDoc">
+		<li>
+			<?=Html::anchor(
+				$dir_docs_educational_standard.'ek-38.03.01-2015.pdf',
+				'38.03.01 Экономика (2015)',
+				['target' => '_blank', 'itemprop' => 'eduFedDoc'])?>;
+		</li>
+		<li>
+			<?=Html::anchor(
+				$dir_docs_educational_standard.'ek-38.03.01-2020.pdf',
+				'38.03.01 Экономика (2020)',
+				['target' => '_blank', 'itemprop' => 'eduFedDoc'])?>;
+		</li>
+	</ul>
+
+	<ul type="disc" itemprop="eduDoc">
+		<li>
+			<?=Html::anchor(
+				$dir_docs_educational_standard.'mn-38.03.02-2016.pdf',
+				'38.03.02 Менеджмент (2016)',
+				['target' => '_blank', 'itemprop' => 'eduFedDoc'])?>;
+		</li>
+		<li>
+			<?=Html::anchor(
+				$dir_docs_educational_standard.'mn-38.03.02-2020.pdf',
+				'38.03.02 Менеджмент (2020)',
+				['target' => '_blank', 'itemprop' => 'eduFedDoc'])?>;
+		</li>
+	</ul>
 </div>
