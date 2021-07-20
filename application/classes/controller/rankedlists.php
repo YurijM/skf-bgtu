f774147add22481ef32fadfe083a0c8f91a0c84f
YurijM <y-myagkov@mail.ru>
2021-07-20 15:53:41 +0300

    Оптимизирован контроллер для ранжированных списков.

--- application/classes/controller/rankedlists.php
+++ application/classes/controller/rankedlists.php
@@ -12,23 +12,35 @@ class Controller_Rankedlists extends Controller_Base {
     
     $ranked->year = $this->request->param('year');
 
-    $ranked->list0 = ORM::factory('matriculant')
-			->where('year', '=', $ranked->year)
-			->and_where('admission_conditions', '=', 0)
-			->order_by(DB::expr('(points_1 + points_2 + points_3 + points_4 + test + achievement)'), 'DESC')
-			->find_all();
+    $ranked->lists = [];
 
-		$ranked->list1 = ORM::factory('matriculant')
-			->where('year', '=', $ranked->year)
-			->and_where('admission_conditions', '=', 1)
-			->order_by(DB::expr('(points_1 + points_2 + points_3 + points_4 + test + achievement)'), 'DESC')
-			->find_all();
-
-		$ranked->list2 = ORM::factory('matriculant')
-			->where('year', '=', $ranked->year)
-			->and_where('admission_conditions', '=', 2)
-			->order_by(DB::expr('(points_1 + points_2 + points_3 + points_4 + test + achievement)'), 'DESC')
-			->find_all();
+		$ranked->lists[] = [
+			'title' => 'Поступающие в рамках особой квоты',
+			'subtitle' => '',
+			'list' => ORM::factory('matriculant')
+				->where('year', '=', $ranked->year)
+				->and_where('admission_conditions', '=', 0)
+				->order_by(DB::expr('(points_1 + points_2 + points_3 + points_4 + test + achievement)'), 'DESC')
+				->find_all()
+		];
+		$ranked->lists[] = [
+			'title' => 'Поступающие в рамках целевого набора',
+			'subtitle' => '(количество мест в пределах квоты целевого приема по направлению: информатика – 3, строительство – 3)',
+			'list' => ORM::factory('matriculant')
+				->where('year', '=', $ranked->year)
+				->and_where('admission_conditions', '=', 1)
+				->order_by(DB::expr('(points_1 + points_2 + points_3 + points_4 + test + achievement)'), 'DESC')
+				->find_all()
+		];
+		$ranked->lists[] = [
+			'title' => 'Поступающие в рамках общего конкурса',
+			'subtitle' => '(количество мест по общему конкурсу: строительство – 24, информатика – 19)',
+			'list' => ORM::factory('matriculant')
+				->where('year', '=', $ranked->year)
+				->and_where('admission_conditions', '=', 2)
+				->order_by(DB::expr('(points_1 + points_2 + points_3 + points_4 + test + achievement)'), 'DESC')
+				->find_all()
+		];
 
 		$ranked->year = $this->request->param('year');
 
--- application/views/v_ranked_lists.php
+++ application/views/v_ranked_lists.php
@@ -32,127 +32,48 @@
 <div class="matriculants">
 	<h2 class="text-center"><?= $page_title . ' (' . $year . 'г.)' ?></h2>
 
-	<h4 class="text-center">
-		Поступающие в рамках особой квоты
-	</h4>
+	<? foreach ($lists as $item): ?>
+		<h4 class="text-center">
+			<?= $item['title'] ?>
+		</h4>
+		<? if ($item['subtitle']): ?>
+			<p><?= $item['subtitle'] ?></p>
+		<? endif ?>
 
-	<? if (count($list0) > 0): ?>
-		<? $n = 1 ?>
-		<div class="table-responsive table-width">
-			<table class="table table-bordered table-condensed bg-info">
-				<tr>
-					<th width="3%">№ п/п</th>
-					<th width="27%">Поступающий</th>
-					<th width="5%">Сумма баллов</th>
-					<th width="5%">Согласие получено</th>
-					<th width="60%">Примечание</th>
-				</tr>
-
-				<? foreach ($list0 as $list): ?>
+		<? if (count($item['list']) > 0): ?>
+			<? $n = 1 ?>
+			<div class="table-responsive table-width">
+				<table class="table table-bordered table-condensed bg-info">
 					<tr>
-						<td class="text-center"><?= $n++ ?></td>
-						<td class="text-center">
-							<?= $list->insurance_number ?>
-						</td>
-						<td class="text-center">
-							<?= $list->points_1 + $list->points_2 + $list->points_3 + $list->points_4 + $list->test + $list->achievement ?>
-						</td>
-						<td class="text-center">
-							<b><?= $list->consent == 0 ? '' : '&check;' ?></b>
-						</td>
-						<td>
-							<?= $list->remark ?>
-						</td>
+						<th width="3%">№ п/п</th>
+						<th width="27%">Поступающий</th>
+						<th width="5%">Сумма баллов</th>
+						<th width="5%">Согласие получено</th>
+						<th width="60%">Примечание</th>
 					</tr>
-				<? endforeach ?>
-			</table>
-		</div>
-	<? else: ?>
-		<h4 class="text-center" style="color: #00F">Нет</h4>
-	<? endif ?>
 
-	<h4 class="text-center">
-		Поступающие в рамках целевого набора
-	</h4>
-	<p>
-		(количество мест в пределах квоты целевого приема по направлению: информатика – 3, строительство – 3)
-	</p>
-
-	<? if (count($list1) > 0): ?>
-		<? $n = 1 ?>
-		<div class="table-responsive table-width">
-			<table class="table table-bordered table-condensed bg-info">
-				<tr>
-					<th width="3%">№ п/п</th>
-					<th width="27%">Поступающий</th>
-					<th width="5%">Сумма баллов</th>
-					<th width="5%">Согласие получено</th>
-					<th width="60%">Примечание</th>
-				</tr>
-
-				<? foreach ($list1 as $list): ?>
-					<tr>
-						<td class="text-center"><?= $n++ ?></td>
-						<td class="text-center">
-							<?= $list->insurance_number ?>
-						</td>
-						<td class="text-center">
-							<?= $list->points_1 + $list->points_2 + $list->points_3 + $list->points_4 + $list->test + $list->achievement ?>
-						</td>
-						<td class="text-center">
-							<b><?= $list->consent == 0 ? '' : '&check;' ?></b>
-						</td>
-						<td>
-							<?= $list->remark ?>
-						</td>
-					</tr>
-				<? endforeach ?>
-			</table>
-		</div>
-	<? else: ?>
-		<h4 class="text-center" style="color: #00F">Нет</h4>
-	<? endif ?>
-
-	<h4 class="text-center">
-		Поступающие в рамках общего конкурса
-	</h4>
-	<p>
-		(количество мест по общему конкурсу: строительство – 24, информатика – 19)
-	</p>
-
-	<? if (count($list2) > 0): ?>
-		<? $n = 1 ?>
-		<div class="table-responsive table-width">
-			<table class="table table-bordered table-condensed bg-info">
-				<tr>
-					<th width="3%">№ п/п</th>
-					<th width="27%">Поступающий</th>
-					<th width="5%">Сумма баллов</th>
-					<th width="5%">Согласие получено</th>
-					<th width="60%">Примечание</th>
-				</tr>
-
-				<? foreach ($list2 as $list): ?>
-					<tr>
-						<td class="text-center"><?= $n++ ?></td>
-						<td class="text-center">
-							<?= $list->insurance_number ?>
-						</td>
-						<td class="text-center">
-							<?= $list->points_1 + $list->points_2 + $list->points_3 + $list->points_4 + $list->test + $list->achievement ?>
-						</td>
-						<td class="text-center">
-							<b><?= $list->consent == 0 ? '' : '&check;' ?></b>
-						</td>
-						<td>
-							<?= $list->remark ?>
-						</td>
-					</tr>
-				<? endforeach ?>
-			</table>
-		</div>
-	<? else: ?>
-		<h4 class="text-center" style="color: #00F">Нет</h4>
-	<? endif ?>
+					<? foreach ($item['list'] as $list): ?>
+						<tr>
+							<td class="text-center"><?= $n++ ?></td>
+							<td class="text-center">
+								<?= $list->insurance_number ?>
+							</td>
+							<td class="text-center">
+								<?= $list->points_1 + $list->points_2 + $list->points_3 + $list->points_4 + $list->test + $list->achievement ?>
+							</td>
+							<td class="text-center">
+								<b><?= $list->consent == 0 ? '' : '&check;' ?></b>
+							</td>
+							<td>
+								<?= $list->remark ?>
+							</td>
+						</tr>
+					<? endforeach ?>
+				</table>
+			</div>
+		<? else: ?>
+			<h4 class="text-center" style="color: #00F">Нет</h4>
+		<? endif ?>
+	<? endforeach ?>
 </div>
 
