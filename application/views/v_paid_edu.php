fe156358ea054baeb8dba0fd4746a21bc6912e0f
YurijM <y-myagkov@mail.ru>
2021-12-28 15:54:55 +0300

    Техзадание от 28.12.2021 10:36 (п.п.1,2)

--- application/views/v_paid_edu.php
+++ application/views/v_paid_edu.php
@@ -1,7 +1,7 @@
 <div class="paid-services row" itemprop="paidEdu">
 	<h2 class="text-center"><?= $page_title ?></h2>
 
-	<ul type="disc">
+	<ul style="list-style-type: disc">
 		<li>
 			<?= HTML::image(
 				$dir_img . 'signature.png',
@@ -65,7 +65,7 @@
 		</strong>
 	</p>
 
-	<ul type="disc">
+	<ul style="list-style-type: disc">
 		<? foreach ($orders as $doc): ?>
 			<? if ($doc['link'] != ''): ?>
 				<li>
@@ -91,7 +91,8 @@
 
 	<p>
 		<strong>
-			Приказы об утверждении стоимости обучения по каждой образовательной программе для среднего профессионального образования:
+			Приказы об утверждении стоимости обучения по каждой образовательной программе для среднего
+			профессионального образования:
 		</strong>
 	</p>
 
@@ -102,7 +103,7 @@
 				[
 					'style' => 'max-width: 1em',
 					'title' => 'Файл подписан простой электронной подписью
-Дата, время: 03.07.2021 14:45
+Дата, время: 03.07.2021 10:30
 ФИО: Курбатов Владимир Леонидович
 Должность: директор
 Уникальный ключ: 5f9e5d79cd1555bedbf271ba7dddc82b7348723b'
@@ -119,25 +120,307 @@
 	</ul>
 
 	<p>
-		<strong>В образовательной организации не реализуются образовательные программы дошкольного, начального общего, основного общего и среднего
-			общего образования.</strong>
+		<strong>
+			В образовательной организации не реализуются образовательные программы дошкольного, начального
+			общего, основного общего и среднего общего образования.
+		</strong>
 	</p>
 
 	<p>
-		<b>Документ об установлении размера платы, взимаемой с родителей (законных представителей) за присмотр и уход за детьми, осваивающими
-			образовательные программы дошкольного образования в организациях, осуществляющих образовательную деятельность, за содержание детей в
-			образовательной организации, реализующей образовательные программы начального общего, основного общего или среднего общего образования,если в
-			такой образовательной организации созданы условия для проживания обучающихся в интернате, либо за осуществление присмотра и ухода за детьми в
-			группах продленного дня в образовательной организации, реализующей образовательные программы начального общего, основного общего или среднего
+		<b>Документ об установлении размера платы, взимаемой с родителей (законных представителей) за присмотр и
+			уход за детьми, осваивающими
+			образовательные программы дошкольного образования в организациях, осуществляющих образовательную
+			деятельность, за содержание детей в
+			образовательной организации, реализующей образовательные программы начального общего, основного общего
+			или среднего общего образования,если в
+			такой образовательной организации созданы условия для проживания обучающихся в интернате, либо за
+			осуществление присмотра и ухода за детьми в
+			группах продленного дня в образовательной организации, реализующей образовательные программы начального
+			общего, основного общего или среднего
 			общего образования:</b> отсутствует
 	</p>
 
-	<div class="col-xs-12 danger" style="border-top: 1px solid #555; margin-bottom: .5em"></div>
+	<h5 class="text-center">
+		Прейскурант
+	</h5>
+
+	<div class="row text-center">
+		<h5 class="col-lg-offset-3 col-md-offset-2 col-sm-offset-1 col-lg-6 col-md-8 col-sm-10 col-xs-12">
+			стоимости платных образовательных услуг на 2021/2022 учебный год для высшего образования по программе
+			подготовки бакалавров
+		</h5>
+	</div>
+
+	<div class="table table-responsive">
+		<table class="table-responsive table-condensed table-bordered">
+			<tr>
+				<th rowspan="3" style="width: 10%">Код</th>
+				<th rowspan="3" style="width: 15%">Направление</th>
+				<th rowspan="3" style="width: 5%">Курс</th>
+				<th colspan="6">Форма обучения</th>
+			</tr>
+			<tr>
+				<th colspan="2">очная</th>
+				<th colspan="2">очно-заочная</th>
+				<th colspan="2">заочная</th>
+			</tr>
+			<tr>
+				<th>Нормативная стоимость</th>
+				<th>Оплата студентов</th>
+				<th>Нормативная стоимость</th>
+				<th>Оплата студентов</th>
+				<th>Нормативная стоимость</th>
+				<th>Оплата студентов</th>
+			</tr>
+
+			<tr class="bg-success text-success">
+				<td class="text-center" rowspan="5">38.03.01</td>
+				<td class="text-center" rowspan="5">Экономика</td>
+				<td class="text-center">1</td>
+				<td class="text-center">98600.00</td>
+				<td class="text-center">72700.00</td>
+				<td class="text-center">47000.00</td>
+				<td class="text-center">47000.00</td>
+				<td class="text-center">36200.00</td>
+				<td class="text-center">36200.00</td>
+			</tr>
+			<tr class="bg-success text-success">
+				<td class="text-center">2</td>
+				<td class="text-center">98600.00</td>
+				<td class="text-center">72700.00</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center">36200.00</td>
+				<td class="text-center">36200.00</td>
+			</tr>
+			<tr class="bg-success text-success">
+				<td class="text-center">3</td>
+				<td class="text-center">98600.00</td>
+				<td class="text-center">72700.00</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center">36200.00</td>
+				<td class="text-center">36200.00</td>
+			</tr>
+			<tr class="bg-success text-success">
+				<td class="text-center">4</td>
+				<td class="text-center">84000.00</td>
+				<td class="text-center">72700.00</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center">36200.00</td>
+				<td class="text-center">36200.00</td>
+			</tr>
+			<tr class="bg-success text-success">
+				<td class="text-center">5</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center">36200.00</td>
+				<td class="text-center">36200.00</td>
+			</tr>
+
+			<tr class="bg-info text-info">
+				<td class="text-center" rowspan="4">38.03.02</td>
+				<td class="text-center" rowspan="4">Менеджмент</td>
+				<td class="text-center">1</td>
+				<td class="text-center">98600.00</td>
+				<td class="text-center">72700.00</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+			</tr>
+			<tr class="bg-info text-info">
+				<td class="text-center">2</td>
+				<td class="text-center">98600.00</td>
+				<td class="text-center">72700.00</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+			</tr>
+			<tr class="bg-info text-info">
+				<td class="text-center">3</td>
+				<td class="text-center">98600.00</td>
+				<td class="text-center">72700.00</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+			</tr>
+			<tr class="bg-info text-info">
+				<td class="text-center">4</td>
+				<td class="text-center">84000.00</td>
+				<td class="text-center">72700.00</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+			</tr>
+
+			<tr class="bg-success text-success">
+				<td class="text-center" rowspan="5">08.03.01</td>
+				<td class="text-center" rowspan="5">Строительство</td>
+				<td class="text-center">1</td>
+				<td class="text-center">115200.00</td>
+				<td class="text-center">83500.00</td>
+				<td class="text-center">54900.00</td>
+				<td class="text-center">54900.00</td>
+				<td class="text-center">39400.00</td>
+				<td class="text-center">39400.00</td>
+			</tr>
+			<tr class="bg-success text-success">
+				<td class="text-center">2</td>
+				<td class="text-center">107920.00</td>
+				<td class="text-center">83500.00</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center">39400.00</td>
+				<td class="text-center">39400.00</td>
+			</tr>
+			<tr class="bg-success text-success">
+				<td class="text-center">3</td>
+				<td class="text-center">107920.00</td>
+				<td class="text-center">83500.00</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center">39400.00</td>
+				<td class="text-center">39400.00</td>
+			</tr>
+			<tr class="bg-success text-success">
+				<td class="text-center">4</td>
+				<td class="text-center">95400.00</td>
+				<td class="text-center">83500.00</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center">39400.00</td>
+				<td class="text-center">39400.00</td>
+			</tr>
+			<tr class="bg-success text-success">
+				<td class="text-center">5</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center">39400.00</td>
+				<td class="text-center">39400.00</td>
+			</tr>
+
+			<tr class="bg-info text-info">
+				<td class="text-center" rowspan="5">09.03.01</td>
+				<td class="text-center" rowspan="5">Информатика и вычислительная техника</td>
+				<td class="text-center">1</td>
+				<td class="text-center">115200.00</td>
+				<td class="text-center">83500.00</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center">41400.00</td>
+				<td class="text-center">41400.00</td>
+			</tr>
+			<tr class="bg-info text-info">
+				<td class="text-center">2</td>
+				<td class="text-center">107920.00</td>
+				<td class="text-center">83500.00</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center">39400.00</td>
+				<td class="text-center">39400.00</td>
+			</tr>
+			<tr class="bg-info text-info">
+				<td class="text-center">3</td>
+				<td class="text-center">107920.00</td>
+				<td class="text-center">83500.00</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center">39400.00</td>
+				<td class="text-center">39400.00</td>
+			</tr>
+			<tr class="bg-info text-info">
+				<td class="text-center">4</td>
+				<td class="text-center">95400.00</td>
+				<td class="text-center">83500.00</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center">39400.00</td>
+				<td class="text-center">39400.00</td>
+			</tr>
+			<tr class="bg-info text-info">
+				<td class="text-center">5</td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center"></td>
+				<td class="text-center">41400.00</td>
+				<td class="text-center">41400.00</td>
+			</tr>
+		</table>
+	</div>
+
+
+	<h5 class="text-center">
+		Прейскурант
+	</h5>
+
+	<div class="row text-center">
+		<h5 class="col-lg-offset-3 col-md-offset-2 col-sm-offset-1 col-lg-6 col-md-8 col-sm-10 col-xs-12">
+			стоимости платных образовательных услуг на 2021/2022 учебный год студентов среднего специального
+			образования
+		</h5>
+	</div>
+
+	<div class="col-md-offset-2 col-md-8 col-sm-offset-1 col-sm-10 col-xs-12">
+		<div class="table table-responsive">
+			<table class="table-responsive table-condensed table-bordered">
+				<tr>
+					<th rowspan="3">Код</th>
+					<th rowspan="3">Направление</th>
+					<th rowspan="3">Курс</th>
+					<th colspan="2">Форма обучения</th>
+				</tr>
+				<tr>
+					<th colspan="2">очная</th>
+				</tr>
+				<tr>
+					<th>Нормативная стоимость</th>
+					<th>Оплата студентов</th>
+				</tr>
+
+				<tr class="bg-success text-success">
+					<td class="text-center">08.01.07</td>
+					<td>Мастер общестроительных работ</td>
+					<td class="text-center">1</td>
+					<td class="text-center">70730.00</td>
+					<td class="text-center">47500.00</td>
+				</tr>
+
+				<tr class="bg-info text-info">
+					<td class="text-center">08.02.11</td>
+					<td>Управление, эксплуатация и обслуживание многоквартирного дома</td>
+					<td class="text-center">1</td>
+					<td class="text-center">72825.00</td>
+					<td class="text-center">46000.00</td>
+				</tr>
+
+				<tr class="bg-success text-success">
+					<td class="text-center">08.03.01</td>
+					<td>Обеспечение информационной безопасности автоматизированных систем</td>
+					<td class="text-center">1</td>
+					<td class="text-center">72825.00</td>
+					<td class="text-center">48000.00</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+
+	<div class="row"></div>
 
 	<p>
 		<strong>Дополнительные образовательные услуги:</strong>
 	</p>
-	<ul type="disc">
+
+	<ul style="list-style-type: disc">
 		<li>
 			<?= Html::anchor('/retraining', 'Профессиональная переподготовка') ?>;
 		</li>
