4cde7cb97547d60bcb757356935bbbef1e2c035d
YurijM <y-myagkov@mail.ru>
2022-02-14 18:51:12 +0300

    Техническое задание от 12.02.2022г.: исправлены ошибки

--- application/classes/controller/specialtiescollege.php
+++ application/classes/controller/specialtiescollege.php
@@ -3,7 +3,7 @@
 class Controller_Specialtiescollege extends Controller_Base {
 	public function action_index()
   {
-    $this->template->page_title = 'Приемная кампания СПО 2021';
+    $this->template->page_title = 'Приемная кампания СПО 2022';
 
     $specialties = View::factory('v_specialties_college');
     
