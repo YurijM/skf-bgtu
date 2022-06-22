d7005122c8563cbb616a6a16c76a42383f2e1b6a
YurijM <y-myagkov@mail.ru>
2022-06-22 17:34:15 +0300

    Техническое задание от 22.06.2022

--- application/views/v_schedule_matriculant.php
+++ application/views/v_schedule_matriculant.php
@@ -11,11 +11,198 @@
 
 		<? /* if (count($intramural) + count($extramural) > 0): */ ?>
 		<p>
-			Для лиц, поступающих по результатам вступительных испытаний, проводимых СКФ БГТУ им.В.Г.Шухова
-			самостоятельно, провести вступительные испытания по графику:
+			Лица, имеющие среднее общее образование (11 классов), поступают в ВУЗ по результатам <b>ЕГЭ</b>.
 		</p>
 
-		<? if (count($intramural) > 0): ?>
+		<h4 class="text-center">График сдачи на ИЮЛЬ</h4>
+
+		<p>
+			1) Лица, имеющие среднее общее образование (11 классов) и относящиеся к инвалидам, лицам с ограниченными возможностями здоровья или к иностранным
+			гражданам, поступающие <b>на очную форму обучения на бюджетные места</b> (в рамках контрольных цифр приема), а также <b>на очную, очно-заочную и
+				заочную формы</b> обучения на места с полным возмещением затрат <b>(платно)</b>, сдают вступительные испытания, проводимые СКФ БГТУ им.В.Г.Шухова,
+			самостоятельно, в соответствии с графиком:
+		</p>
+
+		<div class="table table-responsive row">
+			<table class="table-responsive table-bordered table-condensed bg-info col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 col-xs-12">
+				<tr>
+					<th>Предмет</th>
+					<th>Консультация в 9:00 ауд.10</th>
+					<th>Экзамен в 10:00 ауд.10</th>
+				</tr>
+				<tr>
+					<td>Математика</td>
+					<td class="text-center">15.07.2022 (пт)</td>
+					<td class="text-center"><b>18.07.2022 (пн)</b></td>
+				</tr>
+				<tr>
+					<td>Русский язык</td>
+					<td class="text-center">18.07.2022 (пн)</td>
+					<td class="text-center"><b>19.07.2022 (вт)</b></td>
+				</tr>
+				<tr>
+					<td>Физика</td>
+					<td class="text-center">19.07.2022 (вт)</td>
+					<td class="text-center"><b>20.07.2022 (ср)</b></td>
+				</tr>
+				<tr>
+					<td>Обществознание</td>
+					<td class="text-center">20.07.2022 (ср)</td>
+					<td class="text-center"><b>21.07.2022 (чт)</b></td>
+				</tr>
+				<tr>
+					<td>Информатика</td>
+					<td class="text-center">21.07.2022 (чт)</td>
+					<td class="text-center"><b>22.07.2022 (пт)</b></td>
+				</tr>
+			</table>
+		</div>
+
+		<p>
+			2) Лица, имеющие среднее профессиональное образование, поступающие <b>на очную форму обучения на бюджетные места</b> (в рамках контрольных цифр
+			приема), а также <b>на очную, очно-заочную и заочную формы</b> обучения на места с полным возмещением затрат <b>(платно)</b>, сдают вступительные
+			испытания, проводимые СКФ БГТУ им.В.Г.Шухова, самостоятельно, в соответствии с графиком:
+		</p>
+
+		<div class="table table-responsive row">
+			<table class="table-responsive table-bordered table-condensed bg-info col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 col-xs-12">
+				<tr>
+					<th>Предмет</th>
+					<th>Консультация в 9:00 ауд.10</th>
+					<th>Экзамен в 10:00 ауд.10</th>
+				</tr>
+				<tr>
+					<td>Прикладная математика</td>
+					<td class="text-center">15.07.2022 (пт)</td>
+					<td class="text-center"><b>18.07.2022 (пн)</b></td>
+				</tr>
+				<tr>
+					<td>Русский язык</td>
+					<td class="text-center">18.07.2022 (пн)</td>
+					<td class="text-center"><b>19.07.2022 (вт)</b></td>
+				</tr>
+				<tr>
+					<td>Механика</td>
+					<td class="text-center">19.07.2022 (вт)</td>
+					<td class="text-center"><b>20.07.2022 (ср)</b></td>
+				</tr>
+				<tr>
+					<td>Экономика организации</td>
+					<td class="text-center">20.07.2022 (ср)</td>
+					<td class="text-center"><b>21.07.2022 (чт)</b></td>
+				</tr>
+				<tr>
+					<td>Информационные технологии</td>
+					<td class="text-center">21.07.2022 (чт)</td>
+					<td class="text-center"><b>22.07.2022 (пт)</b></td>
+				</tr>
+			</table>
+		</div>
+
+		<p>
+			<b>Резервный день</b> - 25.07.2022 09:00 (пн)
+		</p>
+		<p>
+			<b>Объявление результатов</b> на следующий день после проведения экзамена, путем размещения на официальном сайте.
+		</p>
+		<p>
+			<b>Апелляция</b> на следующий день после объявления результатов экзамена.
+		</p>
+
+		<h4 class="text-center">График сдачи на АВГУСТ</h4>
+
+		<p>
+			1) Лица, имеющие среднее общее образование (11 классов) и относящиеся к инвалидам, лицам с ограниченными возможностями здоровья или к иностранным
+			гражданам,  поступающие <b>на очную, очно-заочную, заочную формы</b> обучения на места с полным возмещением затрат <b>(платно)</b>, сдают
+			вступительные испытания, проводимые СКФ БГТУ им.В.Г.Шухова, самостоятельно, в соответствии с графиком:
+		</p>
+
+		<div class="table table-responsive row">
+			<table class="table-responsive table-bordered table-condensed bg-success col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 col-xs-12">
+				<tr>
+					<th>Предмет</th>
+					<th>Консультация в 9:00 ауд.10</th>
+					<th>Экзамен в 10:00 ауд.10</th>
+				</tr>
+				<tr>
+					<td>Математика</td>
+					<td class="text-center">12.08.2022 (пт)</td>
+					<td class="text-center"><b>15.08.2022 (пн)</b></td>
+				</tr>
+				<tr>
+					<td>Русский язык</td>
+					<td class="text-center">15.08.2022 (пн)</td>
+					<td class="text-center"><b>16.08.2022 (вт)</b></td>
+				</tr>
+				<tr>
+					<td>Физика</td>
+					<td class="text-center">16.08.2022 (вт)</td>
+					<td class="text-center"><b>17.08.2022 (ср)</b></td>
+				</tr>
+				<tr>
+					<td>Обществознание</td>
+					<td class="text-center">17.08.2022 (ср)</td>
+					<td class="text-center"><b>18.08.2022 (чт)</b></td>
+				</tr>
+				<tr>
+					<td>Информатика</td>
+					<td class="text-center">18.08.2022 (чт)</td>
+					<td class="text-center"><b>19.08.2022 (пт)</b></td>
+				</tr>
+			</table>
+		</div>
+
+		<p>
+			2) Лица, имеющие среднее профессиональное и высшее образование, поступающие <b>на очную, очно-заочную, заочную формы</b> обучения на места с полным
+			возмещением затрат <b>(платно)</b>, сдают  вступительные испытания, проводимые СКФ БГТУ им.В.Г.Шухова, самостоятельно, в соответствии с графиком:
+		</p>
+
+		<div class="table table-responsive row">
+			<table class="table-responsive table-bordered table-condensed bg-success col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 col-xs-12">
+				<tr>
+					<th>Предмет</th>
+					<th>Консультация в 9:00 ауд.10</th>
+					<th>Экзамен в 10:00 ауд.10</th>
+				</tr>
+				<tr>
+					<td>Прикладная математика</td>
+					<td class="text-center">12.08.2022 (пт)</td>
+					<td class="text-center"><b>15.08.2022 (пн)</b></td>
+				</tr>
+				<tr>
+					<td>Русский язык</td>
+					<td class="text-center">15.08.2022 (пн)</td>
+					<td class="text-center"><b>16.08.2022 (вт)</b></td>
+				</tr>
+				<tr>
+					<td>Механика</td>
+					<td class="text-center">16.08.2022 (вт)</td>
+					<td class="text-center"><b>17.08.2022 (ср)</b></td>
+				</tr>
+				<tr>
+					<td>Экономика организации</td>
+					<td class="text-center">17.08.2022 (ср)</td>
+					<td class="text-center"><b>18.08.2022 (чт)</b></td>
+				</tr>
+				<tr>
+					<td>Информационные технологии</td>
+					<td class="text-center">18.08.2022 (чт)</td>
+					<td class="text-center"><b>19.08.2022 (пт)</b></td>
+				</tr>
+			</table>
+		</div>
+
+		<p>
+			<b>Резервные дни</b> - 20.08.2022 09:00 (сб), 27.08.2022 09:00 (сб)
+		</p>
+		<p>
+			<b>Объявление результатов</b> на следующий день после проведения экзамена, путем размещения на официальном сайте.
+		</p>
+		<p>
+			<b>Апелляция</b> на следующий день после объявления результатов экзамена.
+		</p>
+
+		<? /* if (count($intramural) > 0): */ ?><!--
 			<h4 class="text-center">на очную форму обучения на места в рамках КЦП (контрольных цифр приема)</h4>
 			<h4 class="text-center">для инвалидов и лиц с ограниченными возможностями на базе 11 классов</h4>
 
@@ -39,26 +226,26 @@
 						<th>ауд.</th>
 					</tr>
 
-					<? foreach ($intramural as $schedule): ?>
-						<? $subject = ORM::factory('schedulematriculant')->where('education', '=', 0)->and_where('subject', '=', $schedule->subject)->order_by('event_type')->find_all() ?>
+					<? /* foreach ($intramural as $schedule): */ ?>
+						<? /* $subject = ORM::factory('schedulematriculant')->where('education', '=', 0)->and_where('subject', '=', $schedule->subject)->order_by('event_type')->find_all() */ ?>
 
 						<tr>
-							<td><?= $schedule->subject ?></td>
-							<? foreach ($subject as $event_type): ?>
+							<td><? /*= $schedule->subject */ ?></td>
+							<? /* foreach ($subject as $event_type): */ ?>
 								<td class="text-center">
-									<?= $event_type->event_type == 1 ? '<b>' : '' ?>
-									<?= Helper_Addfunction::datetime_from_mysql($event_type->date, FALSE) ?>
-									<?= '(' . $weekdays[date('w', strtotime($event_type->date))] . ')' ?>
-									<?= $event_type->event_type == 1 ? '</b>' : '' ?>
+									<? /*= $event_type->event_type == 1 ? '<b>' : '' */ ?>
+									<? /*= Helper_Addfunction::datetime_from_mysql($event_type->date, FALSE) */ ?>
+									<? /*= '(' . $weekdays[date('w', strtotime($event_type->date))] . ')' */ ?>
+									<? /*= $event_type->event_type == 1 ? '</b>' : '' */ ?>
 								</td>
 								<td class="text-center">
-									<?= $event_type->event_type == 1 ? '<b>' : '' ?>
-									<?= $event_type->classroom ?>
-									<?= $event_type->event_type == 1 ? '</b>' : '' ?>
+									<? /*= $event_type->event_type == 1 ? '<b>' : '' */ ?>
+									<? /*= $event_type->classroom */ ?>
+									<? /*= $event_type->event_type == 1 ? '</b>' : '' */ ?>
 								</td>
-							<? endforeach ?>
+							<? /* endforeach */ ?>
 						</tr>
-					<? endforeach ?>
+					<? /* endforeach */ ?>
 				</table>
 			</div>
 
@@ -147,13 +334,13 @@
 				</table>
 			</div>
 
-			<? if ($intramural_reserve_day->subject): ?>
+			<? /* if ($intramural_reserve_day->subject): */ ?>
 				<p>
-					<em><?= $intramural_reserve_day->subject ?>
-						- <?= Helper_Addfunction::datetime_from_mysql($intramural_reserve_day->date, FALSE) . ' (' . $weekdays[date('w', strtotime($intramural_reserve_day->date))] . ')' ?></em>
+					<em><? /*= $intramural_reserve_day->subject */ ?>
+						- <? /*= Helper_Addfunction::datetime_from_mysql($intramural_reserve_day->date, FALSE) . ' (' . $weekdays[date('w', strtotime($intramural_reserve_day->date))] . ')' */ ?></em>
 				</p>
-			<? endif ?>
-		<? endif ?>
+			<? /* endif */ ?>
+		--><? /* endif */ ?>
 
 		<? if (count($extramural) > 0): ?>
 			<h4 class="text-center">на заочную форму обучения</h4>
@@ -251,10 +438,6 @@
 			<? endif ?>
 		<? endif ?>
 
-		<p>
-			Вступительные испытания на очную, очно-заочную и заочную формы обучения на <b>места с полным возмещением затрат</b> (по договору об оказании платных
-			образовательных услуг) проводятся по субботам (с 02.07.22г. по 27.08.22г., начало экзаменов в 10:00) кабинет №10
-		</p>
 		<? /* else: */ ?><!--
 		<h4 class="text-center" style="margin: 3em 0">Приёмная кампания ещё не началась</h4>
 	--><? /* endif */ ?>
