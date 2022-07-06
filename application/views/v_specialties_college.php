89e8514e2e8194f4100645c81f546333e0273ec8
YurijM <y-myagkov@mail.ru>
2022-07-06 19:40:03 +0300

    Техническое задание от 06.07.2022

--- application/views/v_specialties_college.php
+++ application/views/v_specialties_college.php
@@ -65,6 +65,26 @@
 						['target' => '_blank']
 					) ?>
 				</p>
+				<p>
+					<?= HTML::image(
+						$dir_img . 'signature.png',
+						[
+							'style' => 'max-width: 1em',
+							'title' => 'Файл подписан простой электронной подписью
+Дата, время: 02.05.2022 14:44
+ФИО: Курбатов Владимир Леонидович
+Должность: первый заместитель директора
+Уникальный ключ: 5f9e5d79cd1555bedbf271ba7dddc82b7348723b'
+						]
+					) ?>
+
+
+					<?= HTML::anchor(
+						$dir_docs_programs . 'programm_arh.pdf',
+						'Подготовка к вступительному экзамену',
+						['target' => '_blank']
+					) ?>
+				</p>
 			</div>
 
 			<hr class="visible-xs col-xs-12" style="margin-top: 1em; margin-bottom: 1em;">
