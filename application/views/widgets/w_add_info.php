2fe07049d8e12ae639a5dd387af4dfa405c6b7d9
YurijM <y-myagkov@mail.ru>
2022-12-08 16:39:24 +0300

    Техническое задание от 08.12.2022г.

--- application/views/widgets/w_add_info.php
+++ application/views/widgets/w_add_info.php
@@ -28,12 +28,12 @@
 	<? if ($mode == 'normal'): ?>
 		<div class="row">
 			<div class="col-xs-4 text-center">
-				<?= HTML::anchor('/specialties', 'Приём ВУЗ 2022', [
+				<?= HTML::anchor('/specialties', 'Приём ВУЗ 2023', [
 					'class' => 'btn ' . $btnSize . ' btn-danger'
 				]) ?>
 			</div>
 			<div class="col-xs-4 text-center">
-				<?= HTML::anchor('/specialtiescollege', 'Приём СПО 2022', [
+				<?= HTML::anchor('/specialtiescollege', 'Приём СПО 2023', [
 					'class' => 'btn ' . $btnSize . ' btn-success'
 				]) ?>
 			</div>
