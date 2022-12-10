f7ff249546e8c4280705c1352528d2573724c832
YurijM <y-myagkov@mail.ru>
2022-12-10 20:57:23 +0300

    Техническое задание от 08.12.2022 - заменено описание страниц "Приёмная кампания ВУЗ" и "Приёмная кампания СПО"

--- application/classes/controller/specialtiescollege.php
+++ application/classes/controller/specialtiescollege.php
@@ -3,7 +3,7 @@
 class Controller_Specialtiescollege extends Controller_Base {
 	public function action_index()
   {
-    $this->template->page_title = 'Приемная кампания СПО 2022';
+    $this->template->page_title = 'Приемная кампания СПО 2023';
 
     $specialties = View::factory('v_specialties_college');
     
