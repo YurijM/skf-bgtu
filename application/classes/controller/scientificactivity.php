9e952fed89ade401d5dfdabdcd4753d7ea71645f
YurijM <y-myagkov@mail.ru>
2023-01-18 15:54:58 +0300

    Техническое задание от 18.01.2023г.

--- application/classes/controller/scientificactivity.php
+++ application/classes/controller/scientificactivity.php
@@ -9,9 +9,11 @@ class Controller_Scientificactivity extends Controller_Base {
     $scientificactivity->mode = $this->mode;
     $scientificactivity->page_title = $this->template->page_title;
 
-    /*$scientificactivity->year = DB::select(array('MAX("year")', 'max_year'))->from('scientific_activity')->execute()->get('max_year', 0);
-    
-    $scientificactivity->scientificactivity = ORM::factory('scientificactivity')->with('personnel')->with('personnel:department')->where('year', '=', $scientificactivity->year)->order_by('personnel:department.department')->order_by('personnel.family')->order_by('personnel.name')->order_by('personnel.patronymic')->find_all();*/
+		$scientificactivity->dir_docs = ORM::factory('setting', array('key' => 'dir_docs'))->value;
+
+		/*$scientificactivity->year = DB::select(array('MAX("year")', 'max_year'))->from('scientific_activity')->execute()->get('max_year', 0);
+
+		$scientificactivity->scientificactivity = ORM::factory('scientificactivity')->with('personnel')->with('personnel:department')->where('year', '=', $scientificactivity->year)->order_by('personnel:department.department')->order_by('personnel.family')->order_by('personnel.name')->order_by('personnel.patronymic')->find_all();*/
     
     //$scientificactivity->department = $scientificactivity->scientificactivity[0]->personnel->department->department;
 
