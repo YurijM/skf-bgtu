2faf914b7a1b6f8d878180b500456a2f2ae3bded
YurijM <y-myagkov@mail.ru>
2024-04-03 20:12:09 +0300

    Техническое задание от 03.04.2024г. (окончание)

--- application/classes/controller/achievements.php
+++ application/classes/controller/achievements.php
@@ -3,7 +3,7 @@
 class Controller_Achievements extends Controller_Base {
 	public function action_index()
   {
-    $this->template->page_title = 'Достижения';
+    $this->template->page_title = 'Научно-исследовательская деятельность филиала';
 
     $achievements = View::factory('v_achievements');
     
@@ -14,6 +14,7 @@ class Controller_Achievements extends Controller_Base {
 
     $achievements->site_name = ORM::factory('setting', array('key' => 'site_name'))->value;
     $achievements->dir_img_achievements = ORM::factory('setting', array('key' => 'dir_img_achievements'))->value;
+		$achievements->dir_docs = ORM::factory('setting', array('key' => 'dir_docs'))->value;
 
     $achievements->achievements = ORM::factory('achievement')->order_by('date', 'DESC')->find_all();
 		$achievements->achievements_text = ORM::factory('achievementtext')->find()->limit(1);
--- application/views/v_achievements.php
+++ application/views/v_achievements.php
@@ -1,32 +1,47 @@
 <!-- Скрипты и стили для загрузки галерее-->
-<?=HTML::style($dir_css.'colorbox.css')?>
+<?= HTML::style($dir_css . 'colorbox.css') ?>
 
-<?=HTML::script($dir_js.'jquery.colorbox-min.js')?>
-<?=HTML::script($dir_js.'jquery.colorbox-ru.js')?>
+<?= HTML::script($dir_js . 'jquery.colorbox-min.js') ?>
+<?= HTML::script($dir_js . 'jquery.colorbox-ru.js') ?>
 
 <div class="achievements-list row">
-  <h2 class="text-center"><?=$page_title?></h2>
+	<h2 class="text-center"><?= $page_title ?></h2>
 
 	<div class="col-xs-12 text-justify">
-		<?= Kodoc_Markdown::markdown($achievements_text->content) ?>
+		<div class="col-xs-12">
+			<div class="col-md-4 col-sm-5 hidden-xs <?= ($mode != 'normal' ? 'hidden' : '') ?>">
+				<img src="<?= $dir_img_achievements ?>achievements.jpg" class="img img-responsive" alt="<?= $page_title ?>">
+			</div>
+			<?= Kodoc_Markdown::markdown($achievements_text->content) ?>
+		</div>
+
+		<!--<div class="galery col-xs-12">
+    <? /* foreach ($achievements as $achievement): */ ?>
+      <a class="cboxElement" href="<? /*=$dir_img_achievements.$achievement->img_file*/ ?>">
+        <img src="<? /*=$dir_img_achievements.substr_replace($achievement->img_file, '_mini', strripos($achievement->img_file, '.'), 0)*/ ?>" title="<? /*=Helper_Addfunction::date_from_mysql($achievement->date)*/ ?> - запустить слайдшоу" alt="<? /*=$site_name*/ ?>">
+      </a>
+    <? /* endforeach */ ?>
+  </div>-->
 	</div>
 
-  <div class="galery col-xs-12">
-    <? foreach ($achievements as $achievement): ?>
-      <a class="cboxElement" href="<?=$dir_img_achievements.$achievement->img_file?>">
-        <img src="<?=$dir_img_achievements.substr_replace($achievement->img_file, '_mini', strripos($achievement->img_file, '.'), 0)?>" title="<?=Helper_Addfunction::date_from_mysql($achievement->date)?> - запустить слайдшоу" alt="<?=$site_name?>">
-      </a>
-    <? endforeach ?>
-  </div>
-</div>
-  
-<script>
-  $(document).ready(function() {
-    $(".cboxElement").colorbox({
-      rel: true,
-      slideshow: true,
-      transition: 'elastic',
-      slideshowSpeed: 5000 //5сек
-    });
-  });
-</script>
+	<p>
+		<?= HTML::anchor(
+			$dir_docs . 'itog_nauch.pdf',
+			'Итоги научно-исследовательской деятельности филиала за 2023 год',
+			[
+				'style' => 'text-decoration: underline',
+				'target' => '_blank'
+			]
+		)?>
+	</p>
+
+	<script>
+		$(document).ready(function () {
+			$(".cboxElement").colorbox({
+				rel: true,
+				slideshow: true,
+				transition: 'elastic',
+				slideshowSpeed: 5000 //5сек
+			});
+		});
+	</script>
