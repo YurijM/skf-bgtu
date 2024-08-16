1d4b153fba84758b92e225e644f54af454bbc68e
YurijM <y-myagkov@mail.ru>
2024-08-16 23:05:39 +0300

    Технические задания от 16.08.2024г.

--- application/classes/controller/objects.php
+++ application/classes/controller/objects.php
@@ -3,14 +3,16 @@
 class Controller_Objects extends Controller_Base {
 	public function action_index()
   {
-    $this->template->page_title = 'Материально-техническое обеспечение и оснащенность образовательного процесса';
+    $this->template->page_title = 'Материально-техническое обеспечение и оснащенность образовательного процесса. Доступная среда';
 
     $objects = View::factory('v_objects');
     
     $objects->mode = $this->mode;
     $objects->page_title = $this->template->page_title;
 
-    $objects->dir_docs_logistical_support = ORM::factory('setting', array('key' => 'dir_docs_logistical_support'))
+		$objects->dir_docs = ORM::factory('setting', array('key' => 'dir_docs_docs'))->value;
+		$objects->dir_img = ORM::factory('setting', array('key' => 'dir_img'))->value;
+		$objects->dir_docs_logistical_support = ORM::factory('setting', array('key' => 'dir_docs_logistical_support'))
 			->value;
    
     $this->template->main = $objects;
