e03c8c6fad4227c8690339531020f792256c2d73
YurijM <y-myagkov@mail.ru>
2025-05-07 14:19:00 +0300

    Техническое задание от 07.05.2025г.

--- application/views/v_licensing.php
+++ application/views/v_licensing.php
@@ -1,6 +1,124 @@
 <div class="licensing row">
 	<h2 class="text-center"><?= $page_title ?></h2>
 
+	<h4>Лицензирование специальности 09.02.07 Информационные системы и программирование</h4>
+	<ol>
+		<li>ОПОП 09.02.07</li>
+		<li>Учебный план 09.02.07</li>
+		<li>
+			<?= Html::anchor(
+				$dir_docs_licensing . '3_1 rp_09.02.07.7z',
+				'Рабочие программы дисциплин ',
+				[
+					'target' => '_blank'
+				]
+			) ?>
+			и
+			<?= Html::anchor(
+				$dir_docs_licensing . '3_2 foc_09.02.07.7z',
+				' ФОС 09.02.07',
+				[
+					'target' => '_blank'
+				]
+			) ?>.
+		</li>
+		<li>
+			<?= Html::anchor(
+				$dir_docs_licensing . '4 pp_09.02.07.7z',
+				'Программы практик и ФОС 09.02.07',
+				[
+					'target' => '_blank'
+				]
+			) ?>
+		</li>
+		<li>
+			<?= Html::anchor(
+				$dir_docs_licensing . '5 gia_09.02.07.7z',
+				'ГИА и ФОС 09.02.07',
+				[
+					'target' => '_blank'
+				]
+			) ?>
+		</li>
+		<li>
+			<?= Html::anchor(
+				$dir_docs_licensing . '6 kug_09.02.07.pdf',
+				'Календарный учебный график 09.02.07',
+				[
+					'target' => '_blank'
+				]
+			) ?>
+		</li>
+		<li>
+			<?= Html::anchor(
+				$dir_docs_licensing . '7 dogovor_09.02.07.pdf',
+				'Договор о практической подготовке 09.02.07',
+				[
+					'target' => '_blank'
+				]
+			) ?>
+		</li>
+	</ol>
+
+	<h4>Лицензирование специальности 08.02.08 Монтаж и эксплуатация оборудования и систем газоснабжения</h4>
+	<ol>
+		<li>ОПОП 08.02.08</li>
+		<li>Учебный план 08.02.08</li>
+		<li>
+			<?= Html::anchor(
+				$dir_docs_licensing . '3_1 rp_08.02.08.7z',
+				'Рабочие программы дисциплин ',
+				[
+					'target' => '_blank'
+				]
+			) ?>
+			и
+			<?= Html::anchor(
+				$dir_docs_licensing . '3_2 foc_08.02.08.7z',
+				' ФОС 08.02.08',
+				[
+					'target' => '_blank'
+				]
+			) ?>.
+		</li>
+		<li>
+			<?= Html::anchor(
+				$dir_docs_licensing . '4 pp_08.02.08.7z',
+				'Программы практик и ФОС 08.02.08',
+				[
+					'target' => '_blank'
+				]
+			) ?>
+		</li>
+		<li>
+			<?= Html::anchor(
+				$dir_docs_licensing . '5 gia_08.02.08.7z',
+				'ГИА и ФОС 08.02.08',
+				[
+					'target' => '_blank'
+				]
+			) ?>
+		</li>
+		<li>
+			<?= Html::anchor(
+				$dir_docs_licensing . '6 kug_08.02.08.pdf',
+				'Календарный учебный график 08.02.08',
+				[
+					'target' => '_blank'
+				]
+			) ?>
+		</li>
+		<li>
+			<?= Html::anchor(
+				$dir_docs_licensing . '7 dogovor_08.02.08.pdf',
+				'Договор о практической подготовке 08.02.08',
+				[
+					'target' => '_blank'
+				]
+			) ?>
+		</li>
+	</ol>
+
 	<h4>Лицензирование специальности 54.02.01 Дизайн (по отраслям)</h4>
 	<ol>
 		<li>
@@ -74,4 +192,5 @@
 				]
 			) ?>
 		</li>
+	</ol>
 </div>
