3daf0b8f1a01d1363172293e9cdbb3a58c719fc0
YurijM <y-myagkov@mail.ru>
2026-04-13 18:35:11 +0300

    Техническое задание от 13.04.2026.

--- application/classes/controller/antiterror.php
+++ application/classes/controller/antiterror.php
@@ -11,6 +11,7 @@ class Controller_Antiterror extends Controller_Base {
     $antiterror->page_title = $this->template->page_title;
 
 		$antiterror->dir_docs = ORM::factory('setting', array('key' => 'dir_docs_antiterror'))->value;
+		$antiterror->dir_img = ORM::factory('setting', array('key' => 'dir_img'))->value;
 
 		$this->template->main = $antiterror;
   }
